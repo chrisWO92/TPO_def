814e73f3edbd02901ee14667b3628d115dec8a3d
cristian <chrisworkingonline@gmail.com>
2021-10-21 16:36:13 -0300

    211021 commit

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
    agregarle funcionalidades, como abrir y cerrar
    el menú en los dispositivos móviles.*/
 
-const menu = document.querySelector(".nav-right")
-const openMenuBtn = document.querySelector(".open-menu")
-const closeMenuBtn = document.querySelector(".close-menu")
+const menu = document.querySelector(".nav-right");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
 
 
 /* Función toggle para que cuando esté el menú 
